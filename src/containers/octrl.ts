1c7fa8741ea2f4a2680bf6b71340cd2738985956
t7yang <t7yang@gmail.com>
2019-04-22 08:39:41 +0800

    fix: octrl should handle none Object input

--- src/containers/octrl.ts
+++ src/containers/octrl.ts
@@ -1,14 +1,16 @@
-import { all, map, not, prop, values } from 'ramda';
-import { isType } from '../validators/is-type';
+import { all, equals, gt, is, length, map, not, prop, values } from 'ramda';
 import { Control, Holder } from './type';
 import { devolve } from './_devolve';
 
 type ObjectControl = (struc: Record<string, Control<any>>) => Control<any>;
 
-export const octrl: ObjectControl = (struc: Record<string, Control<any>>) => raw => {
-  const obj = isType(Object) ? raw : {};
+export const octrl: ObjectControl = (struc: Record<string, Control<any>>) => value => {
+  const isNotObject = not(is(Object, value));
+  const isInvalid =
+    isNotObject || (equals(0, length(values(struc))) && gt(length(values(value)), 0));
+  const obj = isNotObject ? {} : value;
   const strucCtrl: Record<string, Holder<any>> = devolve(struc, obj);
-  const valid = all(Boolean, map(prop('valid'), values(strucCtrl)));
+  const valid = isInvalid ? false : all(Boolean, map(prop('valid'), values(strucCtrl)));
   const invalid = not(valid);
   const getValue = () => map<any, any>(({ getValue }) => getValue(), strucCtrl);
   return { valid, invalid, getValue };
