0814b266c173ac012a19f589d9fd4152f580ccd0
t7yang <t7yang@gmail.com>
2019-05-05 23:57:27 +0800

    fix: devolve internal type

--- src/controllers/shared/devolve.ts
+++ src/controllers/shared/devolve.ts
@@ -1,6 +1,7 @@
-import { Control } from '../type';
+import { Control, Holder } from '../type';
+
 export const devolve = (struc: Record<string, Control<any>>, raw: any = {}) => {
-  const result: Record<string, any> = {};
+  const result: Record<string, Holder<any>> = {};
   Object.entries(struc).forEach(([key, ctrl]) => {
     result[key] = ctrl(raw[key]);
   });
