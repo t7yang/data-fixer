a3e8fd2c1537cbdfb537107c9ae8aedb6b3bbe1d
t7yang <t7yang@gmail.com>
2020-04-22 20:54:45 +0800

    refactor: make sure valid in vctrl as boolean

--- src/vctrl/vctrl.ts
+++ src/vctrl/vctrl.ts
@@ -6,6 +6,6 @@ export type Vtor = (data: any) => boolean;
 export type ValueControl = <T>(vtor: Vtor, alt: Alt<T>) => Control<T>;
 
 export const vctrl: ValueControl = (vtor, alt) => data => {
-  const valid = vtor(data);
+  const valid = !!vtor(data);
   return { valid, invalid: !valid, value: () => (valid ? data : getAlt(alt, data)) };
 };
