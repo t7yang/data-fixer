ce5e6e579feaecfbd72ac3ac6c74a53f82d2c73a
t7yang <t7yang@gmail.com>
2020-12-24 10:36:54 +0800

    refactor: fix typings issue for yupAdt due update

--- src/adapter/yup.ts
+++ src/adapter/yup.ts
@@ -1,6 +1,7 @@
-import { Schema, ValidateOptions } from 'yup';
+import { SchemaOf } from 'yup';
+import { ValidateOptions } from 'yup/lib/types';
 import { Vtor } from '../vctrl';
 
-export const yupAdt = (schema: Schema<any>, opt?: ValidateOptions): Vtor => data => {
+export const yupAdt = (schema: SchemaOf<any>, opt?: ValidateOptions): Vtor => data => {
   return schema.isValidSync(data, opt);
 };
